c9e44b54e7bd6487df9fd2d910548dae01ecb9f9
Mahmoud hammad <mahmoudhamad837@gmail.com>
2024-09-12 17:56:19 +0300

    adding new pages

--- app/[locale]/compontents/ui/blocs-renderer/BlockRenderer.tsx
+++ app/[locale]/compontents/ui/blocs-renderer/BlockRenderer.tsx
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
+import Link from 'next/link';
 
 const BlocksRendererComponent = ({ content, classes = "" }:any) => {
   return (
@@ -14,6 +15,26 @@ const BlocksRendererComponent = ({ content, classes = "" }:any) => {
             {children}
           </p>
         ),
+        heading: ({ children, level }) => {
+          switch (level) {
+            case 1:
+              return <h1 className='text-paragraph text-4xl font-bold'>{children}</h1>
+            case 2:
+              return <h2 className='text-paragraph text-3xl font-semibold'>{children}</h2>
+            case 3:
+              return <h3 className='text-paragraph text-2xl font-medium'>{children}</h3>
+            case 4:
+              return <h4 className='text-paragraph text-xl font-normal'>{children}</h4>
+            case 5:
+              return <h5 className='text-paragraph text-lg font-light'>{children}</h5>
+            case 6:
+              return <h6 className='text-paragraph text-base font-thin'>{children}</h6>
+            default:
+              return <h1 className='text-paragraph text-4xl font-bold'>{children}</h1>
+          }
+        },
+        link: ({ children, url }) => <Link href={url}>{children}</Link>,
+        list: ({ children, format }) => format === 'unordered' ? <ul className='text-paragraph text-base font-normal'>{children}</ul> : <ol className='text-paragraph text-base font-normal'>{children}</ol>
       }}
     />
   );
