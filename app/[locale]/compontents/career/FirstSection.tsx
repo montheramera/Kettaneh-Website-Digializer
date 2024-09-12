43710182ce44eea42873b3b7a6442038caf2e76d
Mahmoud hammad <mahmoudhamad837@gmail.com>
2024-09-12 14:12:45 +0300

    fixing no wrap for text

--- app/[locale]/compontents/career/FirstSection.tsx
+++ app/[locale]/compontents/career/FirstSection.tsx
@@ -52,7 +52,7 @@ export default function JobListings({careers}: any) {
                     color: career?.attributes.category.data.attributes.category.btn_color,
                     border: `1px solid ${career?.attributes.category.data.attributes.category.btn_color}`,
                   }}
-                  className={`ml-2 border-[${career?.attributes.category.data.attributes.category.btn_color}] border p-2 rounded-3xl text-[14px] font-[500] leading-[20px] text-[${career?.attributes.category.data.attributes.category.btn_color}] bg-[${career?.attributes.category.data.attributes.category.background_color}]`}
+                  className={`ml-2 border-[${career?.attributes.category.data.attributes.category.btn_color}] border p-2 rounded-3xl text-[14px] font-[500] leading-[20px] text-[${career?.attributes.category.data.attributes.category.btn_color}] bg-[${career?.attributes.category.data.attributes.category.background_color}] whitespace-nowrap`}
                 >
                   {career?.attributes?.category?.data?.attributes?.title} Department
                 </span>
