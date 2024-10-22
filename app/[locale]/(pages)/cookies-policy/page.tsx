56bb60d21d05bff1eb9fda3c8e64dcb8b81c32ef
Mahmoud hammad <development@digializer.com>
2024-10-22 11:35:20 +0300

    linking footer pages with single types

--- app/[locale]/(pages)/cookies-policy/page.tsx
+++ app/[locale]/(pages)/cookies-policy/page.tsx
@@ -4,17 +4,92 @@ import dynamic from 'next/dynamic';
 import ParagraphSkeleton from '@/compontents/ui/skeleton/ParagrapgSkeleton';
 
 
-const API_URL = process.env.NEXT_PUBLIC_STRAPI_BASE_URL
+const API_URL = process.env.NEXT_PUBLIC_STRAPI_BASE_URL;
+
+type Props = {
+  params: { title: string, description: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata({ params }: Props) {
+  try {
+    const res = await fetch(`${API_URL}/api/cookies-policy?populate=seo.fav_icon`, {
+      cache: "no-store",
+    })
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`)
+    }
+
+    const data = await res.json()
+    const seo = data.data?.attributes?.seo || {}
+    const title = seo.meta_title || 'Default Title'
+    const description = seo.meta_description || 'Default Description'
+    const favicon = seo.fav_icon?.data?.attributes?.url || '/default-favicon.ico'
+    const url = seo.link || 'https://example.com'
+    // const siteName = seo.site_name || 'Your Site Name'
+    // const locale = seo.locale || 'en_US'
+    // const type = seo.type || 'website'
+    // const twitterHandle = seo.twitter_handle || '@yourtwitterhandle'
+
+    return {
+      title,
+      description,
+      icons: {
+        icon: favicon,
+        shortcut: favicon,
+        apple: favicon,
+      },
+      
+    }
+  } catch (error) {
+    console.error('Error fetching metadata:', error)
+
+    // Return default metadata if there's an error
+    return {
+      title: 'Default Title',
+      description: 'Default Description',
+      icons: {
+        icon: '/default-favicon.ico',
+        shortcut: '/default-favicon.ico',
+        apple: '/default-favicon.ico',
+      },
+      metadataBase: new URL('https://example.com'),
+      alternates: {
+        canonical: 'https://example.com',
+      },
+      openGraph: {
+        title: 'Default Title',
+        description: 'Default Description',
+        url: 'https://example.com',
+        siteName: 'Your Site Name',
+        locale: 'en_US',
+        type: 'website',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: 'Default Title',
+        description: 'Default Description',
+        site: '@yourtwitterhandle',
+        creator: '@yourtwitterhandle',
+      },
+      other: {
+        'og:image': '/default-og-image.jpg',
+        'twitter:image': '/default-twitter-image.jpg',
+      },
+    }
+  }
+}
 
 const fetchCookiesData = async () => {
-  const res = await fetch(`${API_URL}/api/pages?populate=*&filters[title][$eq]=Cookies Policy`, {
+  const res = await fetch(`${API_URL}/api/cookies-policy?populate=*`, {
     method: "GET",
     headers: {
       "Cache-Control": "no-store", // Specify cache control header
     },
   });
   const data = await res.json();
-  const cookies = data.data.map((el: any)=> el.attributes);
+  const cookies = data.data.attributes;
   return cookies;
 };
 
@@ -40,12 +115,12 @@ const page = async() => {
               <div className="mb-[24px] mt-[24px] flex">
                 <div className="bg-primary min-w-[5px] w-[5px] min-h-[100%] mr-[10px]"></div>
                 <span className="bg-primary text-white py-2 px-4 inline-block text-[21.86px]  font-[400] leading-[22px] uppercase">
-                  {cookiesData[0]?.title}
+                  {cookiesData?.title}
                 </span>
               </div>
        
               <Suspense fallback={"loading"}>
-                <DynamicParagraph content={cookiesData[0]?.text} classes="text-[18px] lg:text-[20.1px] font-[400] leading-[28px] text-paragraph lg:my-4 lg:max-w-[1216px]" />
+                <DynamicParagraph content={cookiesData?.text} classes="text-[18px] lg:text-[20.1px] font-[400] leading-[28px] text-paragraph lg:my-4 lg:max-w-[1216px]" />
               </Suspense>
 
             </div>
--- app/[locale]/(pages)/privacy-policy/page.tsx
+++ app/[locale]/(pages)/privacy-policy/page.tsx
@@ -5,17 +5,92 @@ import dynamic from 'next/dynamic';
 import ParagraphSkeleton from '@/compontents/ui/skeleton/ParagrapgSkeleton';
 
 
-const API_URL = process.env.NEXT_PUBLIC_STRAPI_BASE_URL
+const API_URL = process.env.NEXT_PUBLIC_STRAPI_BASE_URL;
+
+type Props = {
+  params: { title: string, description: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata({ params }: Props) {
+  try {
+    const res = await fetch(`${API_URL}/api/privacy-policy?populate=seo.fav_icon`, {
+      cache: "no-store",
+    })
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`)
+    }
+
+    const data = await res.json()
+    const seo = data.data?.attributes?.seo || {}
+    const title = seo.meta_title || 'Default Title'
+    const description = seo.meta_description || 'Default Description'
+    const favicon = seo.fav_icon?.data?.attributes?.url || '/default-favicon.ico'
+    const url = seo.link || 'https://example.com'
+    // const siteName = seo.site_name || 'Your Site Name'
+    // const locale = seo.locale || 'en_US'
+    // const type = seo.type || 'website'
+    // const twitterHandle = seo.twitter_handle || '@yourtwitterhandle'
+
+    return {
+      title,
+      description,
+      icons: {
+        icon: favicon,
+        shortcut: favicon,
+        apple: favicon,
+      },
+      
+    }
+  } catch (error) {
+    console.error('Error fetching metadata:', error)
+
+    // Return default metadata if there's an error
+    return {
+      title: 'Default Title',
+      description: 'Default Description',
+      icons: {
+        icon: '/default-favicon.ico',
+        shortcut: '/default-favicon.ico',
+        apple: '/default-favicon.ico',
+      },
+      metadataBase: new URL('https://example.com'),
+      alternates: {
+        canonical: 'https://example.com',
+      },
+      openGraph: {
+        title: 'Default Title',
+        description: 'Default Description',
+        url: 'https://example.com',
+        siteName: 'Your Site Name',
+        locale: 'en_US',
+        type: 'website',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: 'Default Title',
+        description: 'Default Description',
+        site: '@yourtwitterhandle',
+        creator: '@yourtwitterhandle',
+      },
+      other: {
+        'og:image': '/default-og-image.jpg',
+        'twitter:image': '/default-twitter-image.jpg',
+      },
+    }
+  }
+}
 
 const fetchPrivacyData = async () => {
-  const res = await fetch(`${API_URL}/api/pages?populate=*&filters[title][$eq]=Privacy Policy`, {
+  const res = await fetch(`${API_URL}/api/privacy-policy?populate=*`, {
     method: "GET",
     headers: {
       "Cache-Control": "no-store", // Specify cache control header
     },
   });
   const data = await res.json();
-  const privacy = data.data.map((el: any)=> el.attributes);
+  const privacy = data.data.attributes;
   return privacy;
 };
 
@@ -41,12 +116,12 @@ const page = async() => {
               <div className="mb-[24px] mt-[24px] flex">
                 <div className="bg-primary min-w-[5px] w-[5px] min-h-[100%] mr-[10px]"></div>
                 <span className="bg-primary text-white py-2 px-4 inline-block text-[21.86px]  font-[400] leading-[22px] uppercase">
-                  {privacyData[0]?.title}
+                  {privacyData?.title}
                 </span>
               </div>
        
               <Suspense fallback={"loading"}>
-                <DynamicParagraph content={privacyData[0]?.text} classes="text-[18px] lg:text-[20.1px] font-[400] leading-[28px] text-paragraph lg:my-4 lg:max-w-[1216px]" />
+                <DynamicParagraph content={privacyData?.text} classes="text-[18px] lg:text-[20.1px] font-[400] leading-[28px] text-paragraph lg:my-4 lg:max-w-[1216px]" />
               </Suspense>
 
             </div>
