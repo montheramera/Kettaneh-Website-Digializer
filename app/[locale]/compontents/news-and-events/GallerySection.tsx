9fa2d5ea5be18e637bc59f3fefc0e6a470ac19e7
mohamed shalaby <development@digializer.com>
2025-09-15 13:12:24 +0300

    update date at events page

--- app/[locale]/compontents/news-and-events/GallerySection.tsx
+++ app/[locale]/compontents/news-and-events/GallerySection.tsx
@@ -3,39 +3,43 @@ import Link from "next/link";
 
 export default function GallerySection({events}:any) {
 
-
   return (
     <div className="font-avenir">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {events.map((event:any, index: number) => (
+        {events.map((event: any, index: number) => (
           <Link
             href={`/en/news-and-events/${event?.title}`}
             key={index}
             aria-label="Go to the News and Events Page"
           >
-          <div
-            className="bg-white shadow-md overflow-hidden"
-          >
-            <Image
-              src={event.Event.image.url}
-              alt={event.Event.title}
-              width={384}
-              height={176}
-              className="object-cover w-full h-auto"
-            />
-            <h3 className="text-[18px] font-[800] leading-[28px] text-heading mx-2 mt-[10px] ">
-              {event.Event.title}
-            </h3>
-            <p className="font-[400] underline mb-[10px] text-[16px] leading-[28px] mx-2 text-[#111928] ">
-              {new Date().toLocaleDateString("en-US", {
+            <div className="bg-white shadow-md overflow-hidden">
+              <Image
+                src={event.Event.image.url}
+                alt={event.Event.title}
+                width={384}
+                height={176}
+                className="object-cover w-full h-auto"
+              />
+              <h3 className="text-[18px] font-[800] leading-[28px] text-heading mx-2 mt-[10px] ">
+                {event.Event.title}
+              </h3>
+              <p className="font-[400] underline mb-[10px] text-[16px] leading-[28px] mx-2 text-[#111928] ">
+                {/* {new Date().toLocaleDateString("en-US", {
                 year: "numeric",
                 month: "long",
                 day: "numeric",
                 hour: "2-digit",
                 minute: "2-digit",
-              })}
-            </p>
-          </div>
+              })} */}
+                {new Date(event.Event.date).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
+            </div>
           </Link>
         ))}
       </div>
